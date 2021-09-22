01a391c17871c7ce7441bf5f580ce5594bd5fa73
ikarona <ljzabyl@yandex.ru>
2021-09-22 17:15:44 +0300

    AthomizeFormula

---
+++ 5.py
@@ -0,0 +1,11 @@
+x = float(input())
+x_2 = x ** 2
+x_3 = x ** 3
+x_2_3 = x_2 - x_3
+x_2_3 = abs( x_2_3 )
+x7 = x * 7
+x15 = x * 15
+xDown = x_3 - x15
+frac = x7 / xDown
+result = x_2_3 - frac
+print( result )
