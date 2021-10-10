dc08fd9a04fb766d7e15623d1ca51a605b88b37b
ikarona <ljzabyl@yandex.ru>
2021-10-10 16:40:43 +0300

    FunVect

--- 11.py
+++ 11.py
@@ -1,15 +1,7 @@
 def superposition(funmod, funseq):
     funres=[]
     for i, funq in enumerate(funseq):
-        def f(funq = funq):
-            nonlocal funres
-            # funres.append(lambda funmod,funq: funmod(funq()))
-            funres.append(funmod(funq()))
-
+        def f(x, funq = funq):
+            return funmod(funq(x))
+        funres.append(f)
     return funres
-
-from math import *
-F = superposition(abs, (sin, cos))
-for i in F:
-    print(i)
-print(F[0](-1), F[1](-1), F[0](2), F[1](2))
